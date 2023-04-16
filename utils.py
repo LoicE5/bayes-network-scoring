ad3d6635ba8f2e48a1763d0e71394bce92b81a19
Loïc <59501884+LoicE5@users.noreply.github.com>
2023-04-17 00:53:28 +0200

    Création du notebook & cleaning

--- utils.py
+++
@@ -1,7 +0,0 @@
-from time import time as now
-from typing import Callable
-
-def show_exec_time(func:Callable, args:list=[])->None:
-    start_time = now()
-    print(f"Output : {func(*args)}")
-    print(f"{func.__name__} have been executed in {round(now() - start_time,3)} seconds")
